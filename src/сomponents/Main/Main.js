96de3d362c5e19958327b55108b5a86b0c6e7731
maria-okoledova394 <maria-okoledova@yandex.ru>
2021-08-24 03:24:05 +0300

    Create adaptive AboutProject component

--- src/сomponents/Main/Main.js
+++ src/сomponents/Main/Main.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../Header/Header';
 import Promo from '../Promo/Promo';
 import NavTab from '../NavTab/NavTab';
+import AboutProject from '../AboutProject/AboutProject';
 
 function Main() {
 
@@ -10,6 +11,7 @@ function Main() {
         <Header loggedIn={false} />
         <Promo />
         <NavTab />
+        <AboutProject />
       </div>
     )
   }
